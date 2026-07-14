alexandreb09/partenaire-raquette

:tada: Project start :tada:

frontend/src/router.js
@@ -1,0 +1,116 @@
+import { useAuthStore } from '@/stores/auth'
+
+export const routes = [
+  {
+    path: '/',
+    component: () => import('@/views/HomeView.vue'),
+    meta: {
+      title: 'Accueil',
+      description: 'Trouvez votre partenaire de tennis sur Sportio. Rejoignez la communauté de joueurs en France, publiez des annonces de parties et trouvez des partenaires de votre niveau. Gratuit.',
+    },
+  },
+  {
+    path: '/joueurs',
+    component: () => import('@/views/PlayersView.vue'),
+    meta: {
+      title: 'Joueurs',
+      description: 'Annuaire des joueurs de tennis disponibles près de chez vous. Filtrez par ville, classement FFT et genre pour trouver le partenaire raquette idéal.',
+    },
+  },
+  {
+    path: '/joueurs/:id',
+    component: () => import('@/views/PlayerProfileView.vue'),
+    meta: {
+      title: 'Profil joueur',
+      description: 'Profil joueur de tennis sur Sportio. Contactez ce joueur pour organiser une partie de tennis près de chez vous.',
+    },
+  },
+  {
+    path: '/annonces',
+    component: () => import('@/views/ProposalsView.vue'),
+    meta: {
+      title: 'Annonces de parties',
+      description: 'Annonces de parties de tennis disponibles partout en France. Rejoignez une partie ou publiez votre annonce pour trouver un partenaire de tennis.',
+    },
+  },
+  {
+    path: '/annonces/nouvelle',
+    component: () => import('@/views/CreateProposalView.vue'),
+    meta: {
+      title: 'Nouvelle annonce',
+      requiresAuth: true,
+    },
+  },
+  {
+    path: '/annonces/:id',
+    component: () => import('@/views/ProposalDetailView.vue'),
+    meta: {
+      title: 'Détail de la partie',
+      description: 'Rejoignez cette partie de tennis ou contactez l\'organisateur. Sportio — trouvez votre partenaire de tennis.',
+    },
+  },
+  {
+    path: '/messages',
+    component: () => import('@/views/MessagesView.vue'),
+    meta: { title: 'Messages', requiresAuth: true },
+  },
+  {
+    path: '/messages/:id',
+    component: () => import('@/views/ConversationView.vue'),
+    meta: { title: 'Conversation', requiresAuth: true },
+  },
+  {
+    path: '/profil',
+    component: () => import('@/views/MyProfileView.vue'),
+    meta: { title: 'Mon profil', requiresAuth: true },
+  },
+  {
+    path: '/connexion',
+    component: () => import('@/views/LoginView.vue'),
+    meta: {
+      title: 'Connexion',
+      description: 'Connectez-vous à Sportio pour accéder à votre espace partenaire de tennis et retrouver vos annonces et messages.',
+    },
+  },
+  {
+    path: '/inscription',
+    component: () => import('@/views/RegisterView.vue'),
+    meta: {
+      title: 'Inscription gratuite',
+      description: 'Créez votre profil gratuitement sur Sportio et trouvez votre partenaire de tennis en quelques minutes. Aucun abonnement requis.',
+    },
+  },
+  {
+    path: '/mot-de-passe-oublie',
+    component: () => import('@/views/ForgotPasswordView.vue'),
+    meta: { title: 'Mot de passe oublié' },
+  },
+  {
+    path: '/reinitialiser-mot-de-passe',
+    component: () => import('@/views/ResetPasswordView.vue'),
+    meta: { title: 'Réinitialiser le mot de passe' },
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: () => import('@/views/NotFoundView.vue'),
+    meta: { title: 'Page introuvable' },
+  },
+]
+
+export function setupRouterGuards(router) {
+  router.beforeEach((to) => {
+    const auth = useAuthStore()
+    if (to.meta.requiresAuth && !auth.isLoggedIn) {
+      return { path: '/connexion', query: { redirect: to.fullPath } }
+    }
+
+    document.title = to.meta.title
+      ? `${to.meta.title} — Sportio`
+      : 'Sportio — Trouvez votre partenaire de tennis en France'
+
+    const metaDesc = document.querySelector('meta[name="description"]')
+    if (metaDesc && to.meta.description) {
+      metaDesc.setAttribute('content', to.meta.description)
+    }
+  })
+}

frontend/src/views/ForgotPasswordView.vue
@@ -1,0 +1,126 @@
+<script setup>
+import { ref } from 'vue'
+import api from '@/api'
+
+const email = ref('')
+const loading = ref(false)
+const sent = ref(false)
+const error = ref('')
+
+async function submit() {
+  error.value = ''
+  if (!email.value.trim()) {
+    error.value = 'Veuillez saisir votre adresse email.'
+    return
+  }
+  loading.value = true
+  try {
+    await api.post('/auth/forgot-password', { email: email.value.trim() })
+    sent.value = true
+  } catch {
+    error.value = 'Une erreur est survenue. Veuillez réessayer.'
+  } finally {
+    loading.value = false
+  }
+}
+</script>
+
+<template>
+  <div class="page-xs">
+    <div class="auth-container">
+      <div class="auth-header">
+        <div class="auth-icon">
+          <v-icon color="white" size="22">mdi-lock-reset</v-icon>
+        </div>
+        <h1 class="auth-title">Mot de passe oublié</h1>
+        <p class="auth-subtitle">
+          {{ sent ? 'Email envoyé !' : 'Saisissez votre email pour recevoir un lien de réinitialisation.' }}
+        </p>
+      </div>
+
+      <div class="auth-card">
+        <!-- Success state -->
+        <div v-if="sent" class="sent-state">
+          <v-icon size="48" color="success" class="mb-3">mdi-check-circle-outline</v-icon>
+          <p class="sent-title">Vérifiez vos emails</p>
+          <p class="sent-desc">
+            Si un compte existe pour <strong>{{ email }}</strong>, vous recevrez un lien
+            de réinitialisation dans quelques minutes.
+          </p>
+          <p class="sent-hint">Pensez à vérifier vos spams si vous ne trouvez pas l'email.</p>
+          <router-link to="/connexion" class="btn-primary auth-submit">
+            Retour à la connexion
+          </router-link>
+        </div>
+
+        <!-- Form state -->
+        <form v-else @submit.prevent="submit">
+          <div v-if="error" class="error-banner">{{ error }}</div>
+
+          <div class="form-field">
+            <label class="field-label">Adresse email</label>
+            <input
+              v-model="email"
+              type="email"
+              required
+              autofocus
+              placeholder="vous@exemple.fr"
+              class="field-input"
+            />
+          </div>
+
+          <button type="submit" :disabled="loading" class="btn-primary auth-submit">
+            <v-progress-circular v-if="loading" size="16" width="2" color="white" indeterminate />
+            <span>{{ loading ? 'Envoi…' : 'Envoyer le lien' }}</span>
+          </button>
+        </form>
+      </div>
+
+      <p class="auth-footer">
+        <router-link to="/connexion" class="auth-footer-link">
+          <v-icon size="13">mdi-arrow-left</v-icon> Retour à la connexion
+        </router-link>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.auth-container { max-width: 400px; margin: 0 auto; padding: 40px 0; }
+
+.auth-header { text-align: center; margin-bottom: 24px; }
+.auth-icon {
+  width: 48px; height: 48px; border-radius: 14px;
+  background: linear-gradient(135deg, var(--c-primary), #D47A52);
+  display: flex; align-items: center; justify-content: center;
+  margin: 0 auto 16px;
+  box-shadow: 0 4px 14px rgba(99,102,241,0.35);
+}
+.auth-title { font-size: 22px; font-weight: 800; color: var(--c-text); letter-spacing: -0.03em; margin: 0 0 6px; }
+.auth-subtitle { font-size: 14px; color: var(--c-text-md); margin: 0; }
+
+.auth-card {
+  background: #fff;
+  border: 1px solid var(--c-border);
+  border-radius: 16px;
+  padding: 28px 24px;
+  margin-bottom: 16px;
+}
+
+.form-field { margin-bottom: 16px; }
+.auth-submit { width: 100%; padding: 11px; font-size: 14px; margin-top: 4px; }
+
+/* Sent state */
+.sent-state { text-align: center; }
+.sent-title { font-size: 16px; font-weight: 700; color: var(--c-text); margin: 0 0 10px; }
+.sent-desc { font-size: 14px; color: var(--c-text-muted); line-height: 1.6; margin: 0 0 8px; }
+.sent-hint { font-size: 12px; color: var(--c-text-sm); margin: 0 0 24px; }
+
+/* Footer */
+.auth-footer { text-align: center; font-size: 13px; color: var(--c-text-md); margin-top: 16px; }
+.auth-footer-link {
+  color: var(--c-primary); font-weight: 600; text-decoration: none;
+  display: inline-flex; align-items: center; gap: 4px;
+}
+.auth-footer-link:hover { text-decoration: underline; }
+</style>

alexandreb09/fervio

frontend/src/router.js
@@ -1,0 +1,116 @@
+import { useAuthStore } from '@/stores/auth'
+
+export const routes = [
+  {
+    path: '/',
+    component: () => import('@/views/HomeView.vue'),
+    meta: {
+      title: 'Accueil',
+      description: 'Trouvez votre partenaire de tennis sur Fervio. Rejoignez la communauté de joueurs en France, publiez des annonces de parties et trouvez des partenaires de votre niveau. Gratuit.',
+    },
+  },
+  {
+    path: '/joueurs',
+    component: () => import('@/views/PlayersView.vue'),
+    meta: {
+      title: 'Joueurs',
+      description: 'Annuaire des joueurs de tennis disponibles près de chez vous. Filtrez par ville, classement FFT et genre pour trouver le partenaire raquette idéal.',
+    },
+  },
+  {
+    path: '/joueurs/:id',
+    component: () => import('@/views/PlayerProfileView.vue'),
+    meta: {
+      title: 'Profil joueur',
+      description: 'Profil joueur de tennis sur Fervio. Contactez ce joueur pour organiser une partie de tennis près de chez vous.',
+    },
+  },
+  {
+    path: '/annonces',
+    component: () => import('@/views/ProposalsView.vue'),
+    meta: {
+      title: 'Annonces de parties',
+      description: 'Annonces de parties de tennis disponibles partout en France. Rejoignez une partie ou publiez votre annonce pour trouver un partenaire de tennis.',
+    },
+  },
+  {
+    path: '/annonces/nouvelle',
+    component: () => import('@/views/CreateProposalView.vue'),
+    meta: {
+      title: 'Nouvelle annonce',
+      requiresAuth: true,
+    },
+  },
+  {
+    path: '/annonces/:id',
+    component: () => import('@/views/ProposalDetailView.vue'),
+    meta: {
+      title: 'Détail de la partie',
+      description: 'Rejoignez cette partie de tennis ou contactez l\'organisateur. Fervio — trouvez votre partenaire de tennis.',
+    },
+  },
+  {
+    path: '/messages',
+    component: () => import('@/views/MessagesView.vue'),
+    meta: { title: 'Messages', requiresAuth: true },
+  },
+  {
+    path: '/messages/:id',
+    component: () => import('@/views/ConversationView.vue'),
+    meta: { title: 'Conversation', requiresAuth: true },
+  },
+  {
+    path: '/profil',
+    component: () => import('@/views/MyProfileView.vue'),
+    meta: { title: 'Mon profil', requiresAuth: true },
+  },
+  {
+    path: '/connexion',
+    component: () => import('@/views/LoginView.vue'),
+    meta: {
+      title: 'Connexion',
+      description: 'Connectez-vous à Fervio pour accéder à votre espace partenaire de tennis et retrouver vos annonces et messages.',
+    },
+  },
+  {
+    path: '/inscription',
+    component: () => import('@/views/RegisterView.vue'),
+    meta: {
+      title: 'Inscription gratuite',
+      description: 'Créez votre profil gratuitement sur Fervio et trouvez votre partenaire de tennis en quelques minutes. Aucun abonnement requis.',
+    },
+  },
+  {
+    path: '/mot-de-passe-oublie',
+    component: () => import('@/views/ForgotPasswordView.vue'),
+    meta: { title: 'Mot de passe oublié' },
+  },
+  {
+    path: '/reinitialiser-mot-de-passe',
+    component: () => import('@/views/ResetPasswordView.vue'),
+    meta: { title: 'Réinitialiser le mot de passe' },
+  },
+  {
+    path: '/:pathMatch(.*)*',
+    component: () => import('@/views/NotFoundView.vue'),
+    meta: { title: 'Page introuvable' },
+  },
+]
+
+export function setupRouterGuards(router) {
+  router.beforeEach((to) => {
+    const auth = useAuthStore()
+    if (to.meta.requiresAuth && !auth.isLoggedIn) {
+      return { path: '/connexion', query: { redirect: to.fullPath } }
+    }
+
+    document.title = to.meta.title
+      ? `${to.meta.title} — Fervio`
+      : 'Fervio — Trouvez votre partenaire de tennis en France'
+
+    const metaDesc = document.querySelector('meta[name="description"]')
+    if (metaDesc && to.meta.description) {
+      metaDesc.setAttribute('content', to.meta.description)
+    }
+  })
+}

frontend/src/views/ForgotPasswordView.vue
@@ -1,0 +1,126 @@
+<script setup>
+import { ref } from 'vue'
+import api from '@/api'
+
+const email = ref('')
+const loading = ref(false)
+const sent = ref(false)
+const error = ref('')
+
+async function submit() {
+  error.value = ''
+  if (!email.value.trim()) {
+    error.value = 'Veuillez saisir votre adresse email.'
+    return
+  }
+  loading.value = true
+  try {
+    await api.post('/auth/forgot-password', { email: email.value.trim() })
+    sent.value = true
+  } catch {
+    error.value = 'Une erreur est survenue. Veuillez réessayer.'
+  } finally {
+    loading.value = false
+  }
+}
+</script>
+
+<template>
+  <div class="page-xs">
+    <div class="auth-container">
+      <div class="auth-header">
+        <div class="auth-icon">
+          <v-icon color="white" size="22">mdi-lock-reset</v-icon>
+        </div>
+        <h1 class="auth-title">Mot de passe oublié</h1>
+        <p class="auth-subtitle">
+          {{ sent ? 'Email envoyé !' : 'Saisissez votre email pour recevoir un lien de réinitialisation.' }}
+        </p>
+      </div>
+
+      <div class="auth-card">
+        <!-- Success state -->
+        <div v-if="sent" class="sent-state">
+          <v-icon size="48" color="success" class="mb-3">mdi-check-circle-outline</v-icon>
+          <p class="sent-title">Vérifiez vos emails</p>
+          <p class="sent-desc">
+            Si un compte existe pour <strong>{{ email }}</strong>, vous recevrez un lien
+            de réinitialisation dans quelques minutes.
+          </p>
+          <p class="sent-hint">Pensez à vérifier vos spams si vous ne trouvez pas l'email.</p>
+          <router-link to="/connexion" class="btn-primary auth-submit">
+            Retour à la connexion
+          </router-link>
+        </div>
+
+        <!-- Form state -->
+        <form v-else @submit.prevent="submit">
+          <div v-if="error" class="error-banner">{{ error }}</div>
+
+          <div class="form-field">
+            <label class="field-label">Adresse email</label>
+            <input
+              v-model="email"
+              type="email"
+              required
+              autofocus
+              placeholder="email@exemple.fr"
+              class="field-input"
+            />
+          </div>
+
+          <button type="submit" :disabled="loading" class="btn-primary auth-submit">
+            <v-progress-circular v-if="loading" size="16" width="2" color="white" indeterminate />
+            <span>{{ loading ? 'Envoi…' : 'Envoyer le lien' }}</span>
+          </button>
+        </form>
+      </div>
+
+      <p class="auth-footer">
+        <router-link to="/connexion" class="auth-footer-link">
+          <v-icon size="13">mdi-arrow-left</v-icon> Retour à la connexion
+        </router-link>
+      </p>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.auth-container { max-width: 400px; margin: 0 auto; padding: 40px 0; }
+
+.auth-header { text-align: center; margin-bottom: 24px; }
+.auth-icon {
+  width: 48px; height: 48px; border-radius: 14px;
+  background: linear-gradient(135deg, var(--c-primary), #D47A52);
+  display: flex; align-items: center; justify-content: center;
+  margin: 0 auto 16px;
+  box-shadow: 0 4px 14px rgba(99,102,241,0.35);
+}
+.auth-title { font-size: 22px; font-weight: 800; color: var(--c-text); letter-spacing: -0.03em; margin: 0 0 6px; }
+.auth-subtitle { font-size: 14px; color: var(--c-text-md); margin: 0; }
+
+.auth-card {
+  background: #fff;
+  border: 1px solid var(--c-border);
+  border-radius: 16px;
+  padding: 28px 24px;
+  margin-bottom: 16px;
+}
+
+.form-field { margin-bottom: 16px; }
+.auth-submit { width: 100%; padding: 11px; font-size: 14px; margin-top: 4px; }
+
+/* Sent state */
+.sent-state { text-align: center; }
+.sent-title { font-size: 16px; font-weight: 700; color: var(--c-text); margin: 0 0 10px; }
+.sent-desc { font-size: 14px; color: var(--c-text-muted); line-height: 1.6; margin: 0 0 8px; }
+.sent-hint { font-size: 12px; color: var(--c-text-sm); margin: 0 0 24px; }
+
+/* Footer */
+.auth-footer { text-align: center; font-size: 13px; color: var(--c-text-md); margin-top: 16px; }
+.auth-footer-link {
+  color: var(--c-primary); font-weight: 600; text-decoration: none;
+  display: inline-flex; align-items: center; gap: 4px;
+}
+.auth-footer-link:hover { text-decoration: underline; }
+</style>